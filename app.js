6c85e322c15ab5b770aa5b0c8401889f4a234376
AnetteeYunuen <anetteyunuenruizm@gmail.com>
2025-10-09 18:58:37 -0600

    Add solution

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var _usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -27,7 +27,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res, _next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
